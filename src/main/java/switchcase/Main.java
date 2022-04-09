d68515e30476f18850b1fd2dd20aba9956017625
TestEngineerU <maryna_romanets@aol.com>
2022-04-09 13:46:47 -0500

    example Switch statement

---
+++ src/main/java/switchcase/Main.java
@@ -0,0 +1,17 @@
+package switchcase;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int stage = 2;
+        //For 13 version
+//        String season = switch(stage) {
+//            case 0 -> "Spring";
+//            case 1 -> "Summer";
+//            case 2 -> "Fall";
+//            case 3 -> "Winter";
+//            default -> "Invalid season";
+//        };
+//        System.out.println(season);
+    }
+}
